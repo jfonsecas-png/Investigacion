4bbf4b1a56580264f70562d9668049431008d6dd
Daniel Fonseca <josedaniel0102@hotmail.com>
2026-03-08 23:14:33 -0600

    Se crea modelo Doctor en proyecto DTO

---
+++ ClinicSystem.DTO/Doctor.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ClinicSystem.DTO
+{
+    public class Doctor
+    {
+        public int Id { get; set; }
+
+        public string Name { get; set; }
+
+        public string Specialty { get; set; }
+
+        public int YearsOfExperience { get; set; }
+    }
+}
