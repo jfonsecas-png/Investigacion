00560706963519c3eb51e2c93368b4d9fa7e9caa
Daniel Fonseca <josedaniel0102@hotmail.com>
2026-03-11 11:28:50 -0600

    Se crea capa AppLogic para Doctors con interfaz IDoctorsManager y clase DoctorsManager

---
+++ ClinicSystem.AppLogic/DoctorsManager.cs
@@ -0,0 +1,25 @@
+﻿using ClinicSystem.DataAccess.Crud;
+using ClinicSystem.DTO;
+
+namespace ClinicSystem.AppLogic
+{
+    public class DoctorsManager : IDoctorsManager
+    {
+        private readonly DoctorCrud _doctorCrud;
+
+        public DoctorsManager(DoctorCrud doctorCrud)
+        {
+            _doctorCrud = doctorCrud;
+        }
+
+        public async Task<List<Doctor>> GetAllDoctorsAsync()
+        {
+            return await _doctorCrud.GetAllAsync();
+        }
+
+        public async Task CreateDoctorAsync(Doctor doctor)
+        {
+            await _doctorCrud.CreateAsync(doctor);
+        }
+    }
+}
---
+++ ClinicSystem.AppLogic/IDoctorsManager.cs
@@ -0,0 +1,10 @@
+﻿using ClinicSystem.DTO;
+
+namespace ClinicSystem.AppLogic
+{
+    public interface IDoctorsManager
+    {
+        Task<List<Doctor>> GetAllDoctorsAsync();
+        Task CreateDoctorAsync(Doctor doctor);
+    }
+}
