9448a646bb5a91c9dbeee6535abfa880193c1bdc
Daniel Fonseca <josedaniel0102@hotmail.com>
2026-03-11 22:15:18 -0600

    Finalizacion de la investigacion

--- ClinicSystem.API/Program.cs
+++ ClinicSystem.API/Program.cs
@@ -10,7 +10,9 @@ builder.Services.AddControllers();
 
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+//Mapper al SQL(1)
 builder.Services.AddDbContext<ClinicDbContext>(options =>options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+//Dependecias (2)
 builder.Services.AddScoped<DoctorCrud>();
 builder.Services.AddScoped<IDoctorsManager, DoctorsManager>();
 var app = builder.Build();
